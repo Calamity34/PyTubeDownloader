5126f5f8702cd3ea59b7427ed76350600e900e45
Calamity34 <NikitaYoutube2008@icloud.com>
2020-05-19 21:11:06 +0500

    package rename, gettitle(url)

--- pydownloader/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-def help():
-    print("dummy package")
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='pydownloader',
+    name='pythondl',
     version='1.0',
     description='Download YT videos using this simple tool!',
     author='Calamity34',
     author_email='nick.goloushckin@ya.ru',
-    packages=['pydownloader'],
+    packages=['pythondl'],
     install_requires=['bs4','beautifulsoup4','pytube3','requests'],
     scripts = [
         'scripts/pydownloader'

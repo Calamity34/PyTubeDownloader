85de07b22d928a37d75d7cdaa8c71c0e755c7c98
Calamity34 <NikitaYoutube2008@icloud.com>
2020-05-19 21:26:53 +0500

    getting ready for PYPI launch

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup
 
 setup(
     name='pythondl',
-    version='1.0',
+    version='1.1',
     description='Download YT videos using this simple tool!',
     author='Calamity34',
     author_email='nick.goloushckin@ya.ru',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.8"
+    ],
     packages=['pythondl'],
     install_requires=['bs4','beautifulsoup4','pytube3','requests'],
     scripts = [
